807ddf390dffd0bcba5e7563802638eaefc5dd76
levy193 <shiningstar193@gmail.com>
2016-05-13 05:01:08 +0700

    ver1.02

--- moneylover.sql
+++ moneylover.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2016 at 12:25 AM
+-- Generation Time: May 13, 2016 at 12:00 AM
 -- Server version: 10.1.13-MariaDB
--- PHP Version: 5.5.34
+-- PHP Version: 5.6.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `bills` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` int(11) NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   `currency` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `totalPay` int(11) NOT NULL,
@@ -36,13 +37,6 @@ CREATE TABLE `bills` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `bills`
---
-
-INSERT INTO `bills` (`id`, `name`, `description`, `currency`, `totalPay`, `deadLine`, `created_at`, `updated_at`) VALUES
-(24, 'Hóa đơn tiền nước tháng 2', '', '', 2000000, '2016-02-29', '2016-05-08 14:41:30', '2016-05-08 14:41:30');
 
 -- --------------------------------------------------------
 
@@ -65,25 +59,25 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `user_id`, `type`, `name`, `icon`, `updated_at`, `created_at`) VALUES
-(1163, 4, '1', 'Thưởng', '44.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1164, 4, '1', 'Tiền lãi', '22.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1165, 4, '1', 'Lương', '4.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1166, 4, '1', 'Đươc tặng', '48.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1167, 4, '1', 'Bán đồ', '56.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1168, 4, '1', 'Khoản thu khác', '16.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1169, 4, '2', 'Ăn uống', '27.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1170, 4, '2', 'Di chuyển', '30.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1171, 4, '2', 'Mua sắm', '6.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1172, 4, '2', 'Yêu & Bạn bè', '20.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1173, 4, '2', 'Giải trí', '10.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1174, 4, '2', 'Du lịch', '1.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1175, 4, '2', 'Sức khỏe', '33.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1176, 4, '2', 'Tặng quà', '73.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1177, 4, '2', 'Gia đình', '26.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1178, 4, '2', 'Giáo dục', '62.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1179, 4, '2', 'Đầu tư', '32.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1180, 4, '2', 'Bảo hiểm', '34.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33'),
-(1181, 4, '2', 'Khoản chi khác', '74.png', '2016-05-11 15:22:33', '2016-05-11 15:22:33');
+(1220, 15, '1', 'Thưởng', '44.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1221, 15, '1', 'Tiền lãi', '22.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1222, 15, '1', 'Lương', '4.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1223, 15, '1', 'Đươc tặng', '48.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1224, 15, '1', 'Bán đồ', '56.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1225, 15, '1', 'Khoản thu khác', '16.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1226, 15, '2', 'Ăn uống', '27.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1227, 15, '2', 'Di chuyển', '30.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1228, 15, '2', 'Mua sắm', '6.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1229, 15, '2', 'Yêu & Bạn bè', '20.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1230, 15, '2', 'Giải trí', '10.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1231, 15, '2', 'Du lịch', '1.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1232, 15, '2', 'Sức khỏe', '33.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1233, 15, '2', 'Tặng quà', '73.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1234, 15, '2', 'Gia đình', '26.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1235, 15, '2', 'Giáo dục', '62.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1236, 15, '2', 'Đầu tư', '32.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1237, 15, '2', 'Bảo hiểm', '34.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(1238, 15, '2', 'Khoản chi khác', '74.png', '2016-05-12 12:28:26', '2016-05-12 12:28:26');
 
 -- --------------------------------------------------------
 
@@ -140,8 +134,8 @@ CREATE TABLE `default_wallets` (
 --
 
 INSERT INTO `default_wallets` (`id`, `name`, `icon`) VALUES
-(1, 'Tiền mặt', 'Banknotes-96.png'),
-(2, 'TK Ngân Hàng', 'Bank Cards-96.png');
+(1, 'Tiền mặt', '44.png'),
+(2, 'TK Ngân Hàng', '3.png');
 
 -- --------------------------------------------------------
 
@@ -295,12 +289,7 @@ CREATE TABLE `transactions` (
 --
 
 INSERT INTO `transactions` (`id`, `type`, `totalMoney`, `date`, `note`, `user_id`, `wallet_id`, `created_at`, `updated_at`) VALUES
-(2, 'Chi tiêu', 100000, '2016-05-19', '', 3, 1, '2016-05-04 17:00:00', '2016-05-18 17:00:00'),
-(3, 'Chi tiêu', 150000, '2016-05-28', '', 3, 1, '2016-05-11 17:00:00', '2016-05-26 17:00:00'),
-(4, 'Tiêu dùng', 230000, '2016-06-16', '', 3, 2, '2016-05-10 17:00:00', '2016-05-27 17:00:00'),
-(5, 'Thu nhập', 200000, '0000-00-00', '', 0, 2, '2016-05-09 14:45:48', '2016-05-09 14:45:48'),
-(6, 'Thu nhập', 20300, '2016-05-11', '', 0, 2, '2016-05-09 14:48:43', '2016-05-09 14:48:43'),
-(7, 'ss', 2002020, '2016-05-12', '', 0, 2, '2016-05-09 14:49:08', '2016-05-09 14:49:08');
+(8, 'Sức khỏe', 20000, '2016-05-14', 'Không có gì', 15, 6, '2016-05-12 13:31:49', '2016-05-12 13:31:49');
 
 -- --------------------------------------------------------
 
@@ -326,16 +315,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `sex`, `currency`, `remember_token`, `created_at`, `updated_at`) VALUES
-(4, 'Anavel', 'donglq_58@vnu.edu.vn', '$2y$10$m.t8J8ltbsi9QPmIZv6u2OJOTkq1vOKogsMk4lpOcVRcho3sMSsH2', '', 1, 'vnd', 'ws6cPnjPO2cTyvpar2UrnzUz3xGxzZzyu41cfnhbBVuYvxaqENm5YQ5vatBT', '2016-04-07 00:39:08', '2016-05-08 23:16:00'),
-(5, 'Kien oc cho', 'kien@gmail.com', '$2y$10$8Xl3uAM10vp0tYl7.DQHJuCPqzqKTAF/YpY1w77u.qOHWvNZSBI5O', '', 2, 'pound', NULL, '2016-04-07 01:39:43', '2016-04-07 01:39:49'),
-(6, 'huyen', 'thanhhuyen@gmail.com', '$2y$10$wIfRPMjhaYAAaCXftdgzm.2Q84mRZhtJsZefxpagB9K4sO.9a2/6e', '', 2, 'dollar', 'JSCV4CckeXPu3fApL6PYEnWq1ehqFUg1VGR0174OMZkDKv6Hz1Tv33piYo5B', '2016-04-10 23:13:12', '2016-04-10 23:17:36'),
-(7, 'dong', 'shiningstar193@gmail.com', '$2y$10$IdCM12HSV16PUPP531gZM.1rPaEVqPHZdhWTu13K.zyKtrg8cW2ji', '', 1, 'vnd', 'YnO8fkKITGiMXuatVPIjrk70FeoDlxSnph5We8hyQa5B6B54QaNNv6Lc4tSX', '2016-04-10 23:29:13', '2016-04-24 16:27:20'),
-(8, 'chau', 'huyenchau19832014@gmail.com', '$2y$10$Vf51W8K9InZIy8GOoxGLQuUwbqgz239vuXukpDj0gEoEqfKhGHDBW', '', 2, 'dollar', 'AtlUQvGJPTW620PaCZhGkiiJVzWM4CAbczRXQpMEJp6fvD3HRjfJJgt9HSGq', '2016-04-11 07:58:07', '2016-04-11 07:59:48'),
-(9, 'Luu Quang Dong', 'donglq_58@vnu.edu.vnd', '$2y$10$sj0wD4eAj9CyEHAb/t25ZeMMioayeMNyia.w0f7/1LEuowHLD6hbq', '', 2, 'vnd', 'DKAsPROIgFDeIt4FnpoqLrLFxQBSQ0hz5vyLkXVcDN1F9GzGK3dtguCJpGO9', '2016-04-24 13:37:58', '2016-04-24 13:52:29'),
-(10, 'Dong', 'shiningstar193@yahoo.com', '$2y$10$9O56QPpBVEj5kswz5ChWeO8M0xw4ezM39cIOIWg1aMfReZvXAjR0u', '', 1, 'vnd', NULL, '2016-04-25 22:18:45', '2016-04-25 22:18:52'),
-(11, 'Đông', 'donglq_58@gmail.com', '$2y$10$s2UJIzUOkCT/ZBxmjrx6/.vxrDiOcTlh6kF/oJkPje057B1hHVUnq', '', 1, 'vnd', 'xdh4hYKkhVcbDXr21ZcpWwUQWT59Iul6MILdAu65c9ucZIFkhPLu9M1z5rZu', '2016-04-28 03:46:43', '2016-04-28 03:59:13'),
-(12, 'NeverDie', 'shiningstar1931995@gmail.com', '$2y$10$ELNlBN2a3wmn9N84Y0hMaeHDSiWpJ73jj6BS70lXaPTGFcjN82JTO', '', 1, 'dollar', '9O7EPF48plvgyvH98qgcR1dPRI7kXV6SWMpArA71Cg41fiTILOJhRErG9EBp', '2016-05-08 23:16:30', '2016-05-08 23:19:18'),
-(13, 'MeiLing', 'donglq_5895@gmail.com', '$2y$10$qTtWyAZQ9TzOKcXvdKd2fOw2MAIBrC9Lrf/DG.tEq60kaYTemXxH.', '', 1, 'vnd', NULL, '2016-05-08 23:20:05', '2016-05-08 23:20:10');
+(15, 'Anavel', 'donglq_58@vnu.edu.vn', '$2y$10$oDl.g9sVDbU1nvvNHBIDluyUOJgqB04kHW4Dxigme4mOeIJzXRvcG', '', 1, 'vnd', NULL, '2016-05-12 12:28:23', '2016-05-12 12:28:26');
 
 -- --------------------------------------------------------
 
@@ -357,8 +337,8 @@ CREATE TABLE `wallets` (
 --
 
 INSERT INTO `wallets` (`id`, `name`, `icon`, `user_id`, `created_at`, `updated_at`) VALUES
-(1, 'Tiền mặt', 'Banknotes-96.png', 13, '2016-05-08 23:20:13', '2016-05-08 23:20:13'),
-(2, 'TK Ngân Hàng', 'Bank Cards-96.png', 13, '2016-05-08 23:20:13', '2016-05-08 23:20:13');
+(5, 'Tiền mặt', '44.png', 15, '2016-05-12 12:28:26', '2016-05-12 12:28:26'),
+(6, 'TK Ngân Hàng', '3.png', 15, '2016-05-12 12:28:26', '2016-05-12 12:28:26');
 
 --
 -- Indexes for dumped tables
@@ -428,12 +408,12 @@ ALTER TABLE `wallets`
 -- AUTO_INCREMENT for table `bills`
 --
 ALTER TABLE `bills`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1182;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1239;
 --
 -- AUTO_INCREMENT for table `default_categories`
 --
@@ -453,17 +433,17 @@ ALTER TABLE `icons`
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `wallets`
 --
 ALTER TABLE `wallets`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
